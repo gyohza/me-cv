6832aa22fc60dd10afbd19d1aa2f566fb330ff74
Dan Nagaoka <44220336+gyohza@users.noreply.github.com>
2026-07-23 13:35:34 -0300

    feat: add nuxt + modules

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/a11y',
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/icon',
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/robots',
+    '@nuxtjs/sitemap',
+    '@nuxt/fonts'
+  ]
+})
