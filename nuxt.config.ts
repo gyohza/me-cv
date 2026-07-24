0b04ab85a8b1a7d12a09815d9fe159558493a5b5
Dan Nagaoka <44220336+gyohza@users.noreply.github.com>
2026-07-24 14:29:41 -0300

    fix(tailwind): replace outdated plugin with manually installed v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,21 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/a11y',
     '@nuxt/eslint',
     '@nuxt/hints',
     '@nuxt/icon',
     '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss',
     '@nuxtjs/robots',
     '@nuxtjs/sitemap',
-    '@nuxt/fonts'
-  ]
-})
+    '@nuxt/fonts',
+  ],
+  css: ['assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
