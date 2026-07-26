d6530c64179016c9a598bdbea5ba3e4e0cbf4a85
Dan Nagaoka <44220336+gyohza@users.noreply.github.com>
2026-07-26 20:51:14 -0300

    feat(cv): render header from config data

---
+++ app/layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="bg-accent/15 h-dvh w-dvw">
+    <slot />
+  </main>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-surface min-h-dvh w-5xl py-16 px-10 container m-auto">
+    <AppHeader
+      :first-name="data?.firstName ?? ''"
+      :last-name="data?.lastName ?? ''"
+      :title="data?.title ?? ''"
+      :links="data?.links ?? []"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { CvConfig } from '~/shared/types/config';
+
+const { locale } = useI18n()
+const config = useRuntimeConfig().public.config as unknown as CvConfig;
+
+const data = computed(() => config.data[locale.value]);
+
+</script>
