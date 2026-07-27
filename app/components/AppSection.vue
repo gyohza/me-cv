e826a3024db9549e394ca1bbd2041823548167ea
Dan Nagaoka <44220336+gyohza@users.noreply.github.com>
2026-07-27 17:25:16 -0300

    fix(section): improve contrast

--- app/components/AppSection.vue
+++ app/components/AppSection.vue
@@ -3,14 +3,14 @@
     :aria-labelledby="headingId"
     class="flex flex-col gap-3 pb-8"
   >
-    <div class="flex flex-row items-center gap-2">
+    <div class="flex flex-row items-center gap-4">
       <h2
         :id="headingId"
-        class="text-3xl text-accent text-shadow-content/5 text-shadow-lg font-display lowercase tracking-wide"
+        class="text-4xl text-accent font-display lowercase tracking-widest brightness-95"
       >
         {{ title }}
       </h2>
-      <hr class="inline-block border-b-2 border-accent/20 w-full -mb-1">
+      <hr class="inline-block border-b-2 border-accent/50 w-full -mb-1">
     </div>
     <slot />
   </section>
