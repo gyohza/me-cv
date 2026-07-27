3490d4af4496b5b6b8046af99c063847648e11e2
Dan Nagaoka <44220336+gyohza@users.noreply.github.com>
2026-07-27 16:57:59 -0300

    fix(section): optimize spacing/sizing

--- app/components/AppSection.vue
+++ app/components/AppSection.vue
@@ -3,15 +3,14 @@
     :aria-labelledby="headingId"
     class="flex flex-col gap-3 pb-8"
   >
-    <div class="flex flex-row items-center gap-2 -mx-4">
-      <hr class="inline-block border-b-2 border-accent/20 w-2">
+    <div class="flex flex-row items-center gap-2">
       <h2
         :id="headingId"
         class="text-3xl text-accent text-shadow-content/5 text-shadow-lg font-display lowercase tracking-wide"
       >
         {{ title }}
       </h2>
-      <hr class="inline-block border-b-2 border-accent/20 w-full">
+      <hr class="inline-block border-b-2 border-accent/20 w-full -mb-1">
     </div>
     <slot />
   </section>
