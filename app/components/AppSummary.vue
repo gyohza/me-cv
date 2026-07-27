0a50f3fbcd9b4f0a8c636d073e83989de9f6528a
Dan Nagaoka <44220336+gyohza@users.noreply.github.com>
2026-07-27 17:24:59 -0300

    fix(summary): fine-tune positioning

--- app/components/AppSummary.vue
+++ app/components/AppSummary.vue
@@ -3,19 +3,19 @@
     <Icon
       :name="quoteIconName"
       :size="quoteIconsize"
-      class="absolute top-0 left-1/8 opacity-10 rotate-180 text-accent/60"
+      class="absolute top-0 left-1/12 opacity-10 rotate-180 text-accent/60"
     />
     <p
       v-if="summary"
       itemprop="description"
-      class="text-sm w-4/5 max-w-lg m-auto text-justify whitespace-pre-wrap text-shadow-content/10 text-shadow-lg"
+      class="text-sm w-4/5 max-w-xl m-auto text-justify whitespace-pre-wrap text-shadow-content/10 text-shadow-lg"
     >
       <strong class="sr-only">Summary: </strong>{{ summary }}
     </p>
     <Icon
       :name="quoteIconName"
       :size="quoteIconsize"
-      class="absolute bottom-0 right-1/8 opacity-10 text-accent/60"
+      class="absolute bottom-0 right-1/12 opacity-10 text-accent/60"
     />
   </section>
 </template>
@@ -24,5 +24,5 @@
 defineProps<{ summary: string }>();
 
 const quoteIconName = ref('ph:quotes-fill');
-const quoteIconsize = ref('5rem');
+const quoteIconsize = ref('6rem');
 </script>
